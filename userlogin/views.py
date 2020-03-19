165c48bff7ead1bc723826109701f98743c4149a
Paritosh Malhotra <malhotra.paritosh@gmail.com>
2020-03-19 13:28:44 +0530

    added userExist query

--- userlogin/views.py
+++ userlogin/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import UserData
+from django.http import JsonResponse
 
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = UserData.objects.all().order_by('uid')
     serializer_class = UserSerializer
+
+
+def userExist(request):
+    emailToCheck = request.GET.get('emailToCheck')
+    all_users = UserData.objects.all()
+
+    if emailToCheck:
+        user_present = all_users.filter(email=emailToCheck)
+
+        if user_present.exists():
+            return JsonResponse({emailToCheck:"true"})
+        else:
+            return JsonResponse({emailToCheck:"false"})
+
+    else:
+        return JsonResponse({'data':'not_sent'})
