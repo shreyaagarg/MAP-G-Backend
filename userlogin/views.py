28b405d861566c2e4088342d27f79978067b0bf3
Paritosh Malhotra <malhotra.paritosh@gmail.com>
2020-04-24 01:17:42 +0530

    Fixed errors in updateGamesPlayed

--- userlogin/views.py
+++ userlogin/views.py
@@ -4,6 +4,9 @@ from .serializers import UserSerializer
 from .models import UserData
 from django.http import JsonResponse
 import ast
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
@@ -39,20 +42,32 @@ def userExist(request):
 
 
 
+@require_POST
+@csrf_exempt
 def updateGamesPlayed(request):
-    user_email = request.GET.get('email')
-    games_played_string = request.GET.get('games_played')
-    user_games_played = ast.literal_eval(games_played_string)
 
-    all_users = UserData.objects.all()
+    if request.method== 'POST':
+        #set encoding of string and load the response into json
+        body_unicode = request.body.decode('utf-8')
+        json_data = json.loads(body_unicode)
 
-    if user_email and user_games_played:
-        user_present = all_users.filter(email=user_email)
+        user_email = json_data['email']
+        games_played_string = json_data['games_played']
 
-        if not user_present:
-            return JsonResponse({'Email':'invalid'})
+        games_played_string = games_played_string.replace('true', 'True')
+        games_played_string = games_played_string.replace('false', 'False')
 
-        current_user = all_users.filter(email=user_email).get()
-        current_user.games_played = user_games_played
-        current_user.save()
-        return JsonResponse({'update':'successful'})
+        # print(user_email)
+        # print(games_played_string)
+        user_games_played = list(eval(games_played_string))
+        all_users = UserData.objects.all()
+        
+        if user_email and user_games_played:
+            user_present = all_users.filter(email=user_email)
+            if not user_present:
+                return JsonResponse({'Email':'invalid'})
+                
+            current_user = all_users.filter(email=user_email).get()
+            current_user.games_played = user_games_played
+            current_user.save()
+            return JsonResponse({'update':'successful'})
