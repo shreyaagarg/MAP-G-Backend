f717e93aec37e52bce1fa4cada2f54608a015d11
Paritosh Malhotra <malhotra.paritosh@gmail.com>
2020-04-24 20:22:38 +0530

    changed models in listeningGame and emotionPredictor

--- emotionPredictor/models.py
+++ emotionPredictor/models.py
@@ -6,8 +6,8 @@ from userlogin.models import UserData
 
 class emotionPredictorData(models.Model):
     emailId = models.EmailField(max_length=100, primary_key=True, default='')
-    timeChosen = ArrayField(models.DateTimeField())
-    answers = ArrayField(models.IntegerField())
+    # timeChosen = ArrayField(models.DateTimeField())
+    answers = ArrayField(ArrayField(models.CharField(max_length=100, default='000'), default=list), default = list)
 
     def __str__(self):
         return str(self.emailId)
--- listeningGame/models.py
+++ listeningGame/models.py
@@ -6,13 +6,17 @@ from userlogin.models import UserData
 
 class listeningGameData(models.Model):
     emailId = models.EmailField(max_length=100, primary_key=True, default='')
-    startTime = models.DateTimeField()
-    ans1 = models.DateTimeField()
-    ans2 = models.DateTimeField()
-    ans3 = models.DateTimeField()
-    pauseTimeStart = ArrayField(models.DateTimeField())
-    pauseTimeEnd = ArrayField(models.DateTimeField())
-    adjustTimings = ArrayField(models.DateTimeField())
+    
+    # ans1 = models.DateTimeField()
+    # ans2 = models.DateTimeField()
+    # ans3 = models.DateTimeField()
+    audioStart =  ArrayField(models.CharField(max_length=100, default='000'), default = list)
+    audioPause =  ArrayField(models.CharField(max_length=100, default='000'), default = list)
+    audioEnd =    ArrayField(models.CharField(max_length=100, default='000'), default = list)
+    audioAdjust = ArrayField(models.CharField(max_length=100, default='000'), default = list)
+
+    answers = ArrayField(ArrayField(models.CharField(max_length=100, default='000'), default=list), default = list)
+
 
 
     def __str__(self):
